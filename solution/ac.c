6f0177eb917c27b77bfc3ed8ac1cd73c091d86b3
Sean <seanhuang1228@gmail.com>
2023-06-28 12:58:52 +0800

    fix: remove define int long long

--- solution/ac.c
+++ solution/ac.c
@@ -2,7 +2,7 @@
 #include "unistd.h"
 #include "assert.h"
 
-#define int long long
+#define ll long long
 
 #define MAXN 100000
 #define BLACK 0
@@ -29,19 +29,19 @@
 
 
 typedef struct Node {
-  int price[2], sum[2], chd[2];
-  int color, sz, id, pid;
+  ll price[2], sum[2], chd[2];
+  ll color, sz, id, pid;
 } Node;
 
 Node pool[MAXN + 1];
-int queue[MAXN + 1];
-int fnt = 0, bck = MAXN;
-int rb_root = 0;
-int *rb_root_ptr = &rb_root;
-int id2tid[MAXN + 1];
-int tid2id[MAXN + 1];
+ll queue[MAXN + 1];
+ll fnt = 0, bck = MAXN;
+ll rb_root = 0;
+ll *rb_root_ptr = &rb_root;
+ll id2tid[MAXN + 1];
+ll tid2id[MAXN + 1];
 
-void print_rb_tree(int root, int layer) {
+void print_rb_tree(ll root, ll layer) {
   assert(layer < 10);
   if (layer == 0) {
     printf("f====================\n");
@@ -50,7 +50,7 @@ void print_rb_tree(int root, int layer) {
   if (root == 0)
     return;
   print_rb_tree(GET_NODE(root).chd[LEFT_CHILD], layer + 1);
-  for (int i = 0; i < layer; ++i) {
+  for (ll i = 0; i < layer; ++i) {
     printf(" ");
   }
   printf("%lld, %lld, %s, %lld, %lld, %lld, %lld\n",
@@ -67,8 +67,8 @@ void print_rb_tree(int root, int layer) {
 }
 
 // gen and init a new node
-int new_node (int big, int small, int pid) {
-  int id = queue[fnt];
+ll new_node (ll big, ll small, ll pid) {
+  ll id = queue[fnt];
   INC(fnt);
   pool[id].price[FST] = small;
   pool[id].price[SEC] = big;
@@ -82,7 +82,7 @@ int new_node (int big, int small, int pid) {
   return id;
 }
 
-void node_swap (int a_nid, int b_nid) {
+void node_swap (ll a_nid, ll b_nid) {
   Node tmp = GET_NODE(a_nid);
   GET_NODE(a_nid).price[FST] = GET_NODE(b_nid).price[FST];
   GET_NODE(a_nid).price[SEC] = GET_NODE(b_nid).price[SEC];
@@ -90,7 +90,7 @@ void node_swap (int a_nid, int b_nid) {
   GET_NODE(b_nid).price[FST] = tmp.price[FST];
   GET_NODE(b_nid).price[SEC] = tmp.price[SEC];
 
-  int tmpid = tid2id[a_nid];
+  ll tmpid = tid2id[a_nid];
   tid2id[a_nid] = tid2id[b_nid];
   tid2id[b_nid] = tmpid;
 
@@ -98,8 +98,8 @@ void node_swap (int a_nid, int b_nid) {
   id2tid[tid2id[b_nid]] = b_nid;
 }
 
-void push(int nid) {
-  int lc = CHILD(nid, LEFT_CHILD), rc = CHILD(nid, RIGHT_CHILD);
+void push(ll nid) {
+  ll lc = CHILD(nid, LEFT_CHILD), rc = CHILD(nid, RIGHT_CHILD);
 
   GET_NODE(nid).sz = GET_NODE(lc).sz + GET_NODE(rc).sz + 1;
   GET_NODE(nid).sum[FST] = GET_NODE(lc).sum[FST] + GET_NODE(rc).sum[!(GET_NODE(lc).sz & 1)] + GET_NODE(nid).price[GET_NODE(lc).sz & 1];
@@ -108,7 +108,7 @@ void push(int nid) {
   return;
 }
 
-void push_up(int nid) {
+void push_up(ll nid) {
   push(nid);
   if (PARENT(nid)) {
     push_up(PARENT(nid));
@@ -116,30 +116,30 @@ void push_up(int nid) {
 }
 
 // put the deleted node back to the queue
-void del_node (int tid) {
+void del_node (ll tid) {
   queue[bck] = tid;
   INC(bck);
   return;
 }
 
 // the insert of basic BST
-int insert (int *root, int big, int small, int pid) {
-  int curr_big = pool[*root].price[SEC], curr_small = pool[*root].price[FST];
+ll insert (ll *root, ll big, ll small, ll pid) {
+  ll curr_big = pool[*root].price[SEC], curr_small = pool[*root].price[FST];
   if (*root == 0) {
     *root = new_node(big, small, pid);
     return *root;
   }
   else {
-    int chd = (big - small < curr_big - curr_small);
+    ll chd = (big - small < curr_big - curr_small);
     return insert(&(pool[*root].chd[chd]), big, small, *root);
   }
 }
 
-int delete_nd (int id) {
+ll delete_nd (ll id) {
   if (!GET_NODE(id).chd[LEFT_CHILD] || !GET_NODE(id).chd[RIGHT_CHILD])
     return id;
   else {
-    int tar = GET_NODE(id).chd[LEFT_CHILD];
+    ll tar = GET_NODE(id).chd[LEFT_CHILD];
     while (GET_NODE(tar).chd[RIGHT_CHILD]) {
       tar = GET_NODE(tar).chd[RIGHT_CHILD];
     }
@@ -150,8 +150,8 @@ int delete_nd (int id) {
 }
 
 // rotate, nid is the origin root, and dir is the index of child which will be the new root.
-void rotate(int nid, int dir) {
-  int pid = PARENT(nid), cid = GET_NODE(nid).chd[dir];
+void rotate(ll nid, ll dir) {
+  ll pid = PARENT(nid), cid = GET_NODE(nid).chd[dir];
 
   // link cid to pid
   if (pid)
@@ -176,7 +176,7 @@ void rotate(int nid, int dir) {
 }
 
 // the complex flow controll of rb insertion
-void casely_insert(int nid) {
+void casely_insert(ll nid) {
   if (PARENT(nid) == 0)
     GET_NODE(nid).color = BLACK;
   else if (GET_NODE(PARENT(nid)).color == BLACK)
@@ -199,9 +199,9 @@ void casely_insert(int nid) {
   return;
 }
 
-void casely_delete(int nid) {
+void casely_delete(ll nid) {
   if (PARENT(nid)) {
-    int sibling = SIBLING(nid);
+    ll sibling = SIBLING(nid);
     if (GET_NODE(SIBLING(nid)).color == RED) {
       GET_NODE(PARENT(nid)).color = RED;
       GET_NODE(sibling).color = BLACK;
@@ -238,8 +238,8 @@ void casely_delete(int nid) {
 }
 
 // the insert of r-b tree
-int rb_insert(int *root, int big, int small) {
-  int nid = insert(root, big, small, 0);
+ll rb_insert(ll *root, ll big, ll small) {
+  ll nid = insert(root, big, small, 0);
   casely_insert(nid);
 
   push_up(nid);
@@ -247,10 +247,10 @@ int rb_insert(int *root, int big, int small) {
   return nid;
 }
 
-void rb_delete(int id) {
-  int nid = delete_nd(id);
+void rb_delete(ll id) {
+  ll nid = delete_nd(id);
 
-  int chd = CHILD(nid, NO_LEFT_CHILD(nid));
+  ll chd = CHILD(nid, NO_LEFT_CHILD(nid));
   if (!PARENT(nid)) {
     rb_root = chd;
     GET_NODE(chd).pid = 0;
@@ -275,18 +275,18 @@ void rb_delete(int id) {
   return;
 }
 
-signed main () {
-  int n, m;
+int main () {
+  ll n, m;
   scanf("%lld%lld", &n, &m);
 
   pool[rb_root].color = BLACK;
 
-  for (int i = 1; i <= MAXN; ++i) {
+  for (ll i = 1; i <= MAXN; ++i) {
     queue[i - 1] = i;
   }
 
-  for (int i = 1; i <= n; ++i) {
-    int big, small;
+  for (ll i = 1; i <= n; ++i) {
+    ll big, small;
     scanf("%lld%lld", &big, &small);
     if (big < small)
       big ^= small ^= big ^= small;
@@ -296,13 +296,13 @@ signed main () {
   }
 
   printf("%lld\n", GET_NODE(rb_root).sum[FST]);
-  int prev = GET_NODE(rb_root).sum[FST] % MOD;
+  ll prev = GET_NODE(rb_root).sum[FST] % MOD;
 
-  for (int i = 0; i < m - 1; ++i) {
-    int id, c, d, e, f;
+  for (ll i = 0; i < m - 1; ++i) {
+    ll id, c, d, e, f;
     scanf("%lld%lld%lld%lld%lld", &id, &c, &d, &e, &f);
-    int big = (c * prev + d) % MOD;
-    int small = (e * prev + f) % MOD;
+    ll big = (c * prev + d) % MOD;
+    ll small = (e * prev + f) % MOD;
     if (big < small)
       big ^= small ^= big ^= small;
 
